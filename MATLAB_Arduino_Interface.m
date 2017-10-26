7437a6d3e2e6a3474c59e2dd4d428c603c1aca18
Manthan Shah <manthan711@gmail.com>
2017-10-26 17:19:57 -0400

    Add files via upload

--- MATLAB_Arduino_Interface.m
+++ MATLAB_Arduino_Interface.m
@@ -3,7 +3,7 @@ clear all;
 clc;
 clear;
 %% Create serial object for Arduino
-s = serial('COM9'); % change the COM Port number as needed
+s = serial('/dev/cu.wchusbserial1420'); % change the COM Port number as needed
 set(s, 'BaudRate', 9600);
 set(s, 'DataBits', 8);
 set(s, 'StopBits', 1);
@@ -18,18 +18,30 @@ while(true)
     end
 end
 %% Get User Input
-while(true)
-    command = input('Which command do you want: ');
-    %used to write basic information to the slave (in this case the master
-    %Arduino)
-    fprintf(s, '%i', slave);
-    fwrite(s, 's');
-    fprintf(s, '%i', command);
-    fwrite(s, 's');
-    while(get(s, 'BytesAvailable')==0)
-    end
-    %a=fread(s, 1); %how to get readings from Arduino
+
+% while(get(s, 'BytesAvailable')==0)
+% end
+x = [];
+y = [];
+z = [];
+while(length(x) < 250)
+%     fprintf(s, '%i', slave);
+%     fwrite(s, 's');
+%     fprintf(s, '%i', command);
+%     fwrite(s, 's');
+      while(get(s, 'BytesAvailable')==0)
+      end
+      x_new = fscanf(s, '%d');
+      y_new = fscanf(s, '%d');
+      z_new = fscanf(s, '%d');
+      x = [x, x_new]; %how to get readings from Arduino
+      y = [y, y_new]; %how to get readings from Arduino
+      z = [z, z_new]; %how to get readings from Arduino
+
+      
+      %disp(x_new);
+      %disp(y_new);
+      %disp(z_new);
 end
 disp('Execution Ended');
 fclose(s);
-clear all;
