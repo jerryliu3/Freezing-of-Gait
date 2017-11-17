e5dc645280112473ddf24e0bfb7af06d006565ad
Jerry Liu <3jerryliu@gmail.com>
2017-11-17 17:33:05 -0500

    Add files via upload
    Fully working for current training data, not cleaned up for unnecessary code yet

--- MATLAB_Arduino_Interface_Algorithm_Testing.m
+++ MATLAB_Arduino_Interface_Algorithm_Testing.m
@@ -1,30 +1,13 @@
 close all;
 clc;
-
+%Gz = Gy;
+Gz = -Gz;
 %1 = positive, -1 = negative
 derivative = 0;
-pos_in_signal = 1;
+pos_in_signal = 2;
 start_countdown = false;
-countdown = 10;
+
 last_classification = 0;
-
-while(countdown > 0)
-    if(start_countdown)
-        countdown = countdown - 1;
-    end
-    Ax_new = Ax(pos_in_signal);
-    Ay_new = Ay(pos_in_signal);
-    Az_new = Az(pos_in_signal);
-    Gx_new = Gx(pos_in_signal);
-    Gy_new = Gy(pos_in_signal);
-    Gz_new = Gz(pos_in_signal);
-    pos_in_signal = pos_in_signal + 1;
-    if(Gz_new > 20000)
-        start_countdown = true;
-        last_classification = length(Gz);
-    end
-end
-
 counter = 0;
 
 Gz_max = 0;
@@ -32,7 +15,12 @@ Gz_min = 0;
 min_found = false;
 %Feature 1
 num_peaks = 0;
+average_value = 0;
 
+classification = [1, length(Ax)];
+for i = 1:500
+    classification(1, i) = -1;
+end
 while(pos_in_signal < length(Ax))
     Ax_new = Ax(pos_in_signal);
     Ay_new = Ay(pos_in_signal);
@@ -40,10 +28,11 @@ while(pos_in_signal < length(Ax))
     Gx_new = Gx(pos_in_signal);
     Gy_new = Gy(pos_in_signal);
     Gz_new = Gz(pos_in_signal);
-    %disp(counter);
     %if they are not taking a normal step
     if(Gz_new < 20000)
-        
+%         if(min_found)
+%             average_value = (average_value * counter + Gz_new)/(counter + 1);
+%         end
         %calculate if new max or min
         if(Gz_new > Gz_max && min_found)
             Gz_max = Gz_new;
@@ -54,39 +43,40 @@ while(pos_in_signal < length(Ax))
         
         %ignore when values are constant because saddle points are useless
         %so derivative of 0 has no impact
-        if(Gz(pos_in_signal)-Gz(pos_in_signal-1) > 1200)
+        if(Gz(pos_in_signal)-Gz(pos_in_signal-1) > 500)
             if(derivative == -1)
-                num_peaks = num_peaks+1;
+                %num_peaks = num_peaks+1;
             end
             derivative = 1;
-        elseif(Gz(pos_in_signal)-Gz(pos_in_signal-1) < -1200)
+        elseif(Gz(pos_in_signal)-Gz(pos_in_signal-1) < -500)
             if(derivative == 1)
                 num_peaks = num_peaks+1;
+                average_value = (average_value * counter + Gz_new)/(counter + 1);
             end
             derivative = -1;
         end
-        if(num_peaks >= 6 && Gz_max - Gz_min < 20000 && Gz_max - Gz_min > 10000)
-            disp('Freezing');
-            disp(pos_in_signal);
-            counter = 0;
-            Gz_max = 0;
-            Gz_min = 0;
-            min_found = false;
-            num_peaks = 0;
-        elseif(counter >= 40)
-            disp('Either normal walking or stopping');
-            %classification
-            if(num_peaks <= 3)
-                disp('stopping?');
-            else
-                disp('normal walking');
+        if(num_peaks >= 3)
+            if(average_value < 2000 && average_value > -1000 && Gz_max - Gz_min > 2000 && Gz_max < 14000 && Gz_min > -14000)
+                disp('Freezing');
+                classification(1, pos_in_signal) = 2;
+            elseif(Gz_max - Gz_min < 1500 && Gz_max < 5000 && Gz_min > -5000 || average_value < 0 || Gz_max - Gz_min > 20000)
+                disp('Stopping or walking');
+                classification(1, pos_in_signal) = 1;
             end
             disp(pos_in_signal);
+            disp(average_value);
+            disp(Gz_max - Gz_min);
+            disp(Gz_max);
+            disp(Gz_min);
             counter = 0;
             Gz_max = 0;
             Gz_min = 0;
             min_found = false;
             num_peaks = 0;
+            average_value = 0;
+        elseif(counter >= 40)
+            disp('Either normal walking or stopping');
+            classification(1, pos_in_signal) = 1;
         else
             %disp('Unknown');
         end
@@ -99,6 +89,7 @@ while(pos_in_signal < length(Ax))
         Gz_min = 0;
         min_found = false;
         num_peaks = 0;
+        average_value = 0;
     end
     counter = counter + 1;
     pos_in_signal = pos_in_signal + 1;
@@ -110,6 +101,19 @@ end
 % subplot(3,1,3), plot(Az);xlim([0 data_points]); xlabel('Time'); ylabel('Z Acceleration Value');
 
 figure;
-subplot(3,1,1), plot(Gx);xlim([0 data_points]); xlabel('Time'); ylabel('X Angular Acceleration Value');
-subplot(3,1,2), plot(Gy);xlim([0 data_points]); xlabel('Time'); ylabel('Y Angular Acceleration Value');
-subplot(3,1,3), plot(Gz);xlim([0 data_points]); xlabel('Time'); ylabel('Z Angular Acceleration Value');
+%subplot(3,1,1), plot(Gx);xlim([0 data_points]); xlabel('Time'); ylabel('X Angular Acceleration Value');
+%subplot(3,1,2), plot(Gy);xlim([0 data_points]); xlabel('Time'); ylabel('Y Angular Acceleration Value');
+%subplot(3,1,3), 
+plot(Gz);xlim([0 data_points]); xlabel('Time'); ylabel('Z Angular Acceleration Value');
+hold on;
+for i = 1:500
+    if(classification(1, i) == -1)
+        plot(i, Gz(i), 'blackx', 'MarkerSize', 8);
+    elseif(classification(1, i) == 0)
+        plot(i, Gz(i), 'gx', 'MarkerSize', 12);
+    elseif(classification(1, i) == 1)
+        plot(i, Gz(i), 'gx', 'MarkerSize', 12);
+    else
+        plot(i, Gz(i), 'bx', 'MarkerSize', 20);
+    end
+end
